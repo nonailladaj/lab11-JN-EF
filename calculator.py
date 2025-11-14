09d388f4b58fbe5e1357d11594aa2ac1415fbfb8
James N <nonailladaj@ufl.edu>
2025-11-14 13:32:26 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,20 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
+import math
 def add(a, b): 
-
-
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b/a
+def logarithm(a, b):
+    if a<=0 or a==1
+        raise ValueError
+    if b<=0
+        raise ValueError
+    return math.log(b,a)
+def exponent(a, b):
+    return a**b
 
